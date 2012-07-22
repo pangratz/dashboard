6734c08e8ed014db38c5947337d1acd8c1a9f38c
pangratz <cmueller.418@gmail.com>
2012-07-22 12:29:23 +0200

    Update main to show basic application

--- app/lib/main.js
+++ app/lib/main.js
@@ -1,1 +1,15 @@
 require('dashboard/core');
+require('dashboard/github_data_source');
+require('dashboard/controller');
+
+Dashboard.gitHubDataSource = Dashboard.GitHubDataSource.create();
+Dashboard.set('repositoriesController', Dashboard.RepositoriesController.create({
+  content: [],
+  dataSourceBinding: 'Dashboard.gitHubDataSource'
+}));
+Dashboard.get('repositoriesController').loadWatchedRepositories('pangratz');
+
+Ember.View.create({
+  templateName: 'repositories',
+  controllerBinding: 'Dashboard.repositoriesController'
+}).append();
