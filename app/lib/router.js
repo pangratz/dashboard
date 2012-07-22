2225c3272c9175a901161fcbfb218fd9759f7ae1
pangratz <cmueller.418@gmail.com>
2012-07-22 18:07:34 +0200

    Move assembly from main.js to router

--- app/lib/router.js
+++ app/lib/router.js
@@ -1,9 +1,26 @@
 require('dashboard/core');
+require('dashboard/github_data_source');
 
 Dashboard.Router = Ember.Router.extend({
   root: Ember.Route.extend({
     index: Ember.Route.extend({
-      route: '/'
+      route: '/',
+      connectOutlets: function(router) {
+        // create the data source which connects to GitHub API
+        var dataSource = Dashboard.GitHubDataSource.create();
+
+        // initialize repositoriesController
+        var repositoriesController = router.get('repositoriesController');
+        repositoriesController.set('dataSource', dataSource);
+        repositoriesController.set('content', []);
+        repositoriesController.loadWatchedRepositories('pangratz');
+
+        // finally add a view which renders repositories template for given controller
+        Ember.View.create({
+          templateName: 'repositories',
+          controller: repositoriesController
+        }).append();
+      }
     })
   })
 });
