b66e65a30b0181b486a8c778d61a975c5070875e
pangratz <cmueller.418@gmail.com>
2012-07-22 17:52:22 +0200

    Add ApplicationController, ApplicationView and Router for Dashboard

---
+++ app/lib/router.js
@@ -0,0 +1,3 @@
+require('dashboard/core');
+
+Dashboard.Router = Ember.Router.extend();
---
+++ app/tests/router_tests.js
@@ -0,0 +1,7 @@
+require('dashboard/router');
+
+module("Dashboard.Router");
+
+test("it exists", function() {
+  ok(Dashboard.Router, "it exists");
+});
