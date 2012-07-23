40c54166b685e72b60db29c50b82cc71fe7f9b37
pangratz <cmueller.418@gmail.com>
2012-07-23 16:00:07 +0200

    Add test for Dashboard.RepositoriesView

--- app/tests/view_test.js
+++ app/tests/view_test.js
@@ -5,3 +5,9 @@ module("Dashboard.ApplicationView");
 test("it exists", function() {
   ok(Dashboard.ApplicationView, "it exists");
 });
+
+module("Dashboard.RepositoriesView");
+
+test("it exists", function() {
+  ok(Dashboard.RepositoriesView, "it exists");
+});
