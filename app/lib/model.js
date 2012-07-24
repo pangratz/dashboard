78f26ce83d53fd9459c9f6968b1cbcc025b51d08
pangratz <cmueller.418@gmail.com>
2012-07-24 18:26:46 +0200

    Add Dashboard.Event model

--- app/lib/model.js
+++ app/lib/model.js
@@ -13,3 +13,12 @@ Dashboard.Repository = DS.Model.extend({
     return this.get('data.owner')
   }.property('data')
 });
+
+Dashboard.Event = DS.Model.extend({
+  type: DS.attr('string'),
+  created_at: DS.attr('string'),
+  actor: function() { return this.get('data.actor'); }.property('data.actor'),
+  repo: function() { return this.get('data.repo'); }.property('data.repo'),
+  org: function() { return this.get('data.org'); }.property('data.org'),
+  payload: function() { return this.get('data.payload'); }.property('data.payload')
+});
--- app/tests/unit/model_test.js
+++ app/tests/unit/model_test.js
@@ -6,3 +6,10 @@ test("it exists", function() {
   ok(Dashboard.Repository, "it exists");
   ok(DS.Model.detect(Dashboard.Repository), "it is a DS.Model");
 });
+
+module("Dashboard.Event");
+
+test("it exists", function() {
+  ok(Dashboard.Event, "it exists");
+  ok(DS.Model.detect(Dashboard.Event), "it is a DS.Model");
+});
