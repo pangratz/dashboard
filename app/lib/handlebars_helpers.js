6d067e1f1b198aae3e2edff417edef16ba922738
pangratz <cmueller.418@gmail.com>
2012-07-24 21:11:43 +0200

    Add missing trim helper

--- app/lib/handlebars_helpers.js
+++ app/lib/handlebars_helpers.js
@@ -4,3 +4,13 @@ require('dashboard/core');
 Ember.Handlebars.registerHelper('echo', function(propertyName, options) {
     return Ember.get(options.contexts[0], propertyName);
 });
+
+// trim a given property to specific length
+Ember.Handlebars.registerHelper('trim', function(path, options) {
+    var length = options.hash.length;
+    var string = Ember.get(options.contexts[0], path);
+    if (string && length) {
+        string = string.substring(0, length);
+    }
+    return new Handlebars.SafeString(string || '');
+});
--- app/tests/unit/handlebars_helpers_test.js
+++ app/tests/unit/handlebars_helpers_test.js
@@ -20,3 +20,49 @@ test("returns the specified property", function() {
   
   equal(Ember.$('#qunit-fixture').text(), "dingdong", "echo helper outputs correct value");
 });
+
+module("{{trim}}");
+
+test("trim is available", function() {
+  ok(Ember.Handlebars.helpers.trim, "trim helper is available");
+});
+
+test("outputs by default given string", function() {
+  var view = Ember.View.create({
+    property: 'mystring',
+    template: Ember.Handlebars.compile('{{trim property}}')
+  });
+
+  Ember.run(function() {
+    view.appendTo('#qunit-fixture');
+  });
+
+  equal(Ember.$('#qunit-fixture').text(), 'mystring', "by default doesn't trim anything");
+});
+
+test("trims string to specific length", function() {
+  var view = Ember.View.create({
+    property: 'mystring',
+    template: Ember.Handlebars.compile('{{trim property length=2}}')
+  });
+  
+  Ember.run(function() {
+    view.appendTo('#qunit-fixture');
+  });
+  
+  equal(Ember.$('#qunit-fixture').text(), 'my', "it trims the string to given length");
+});
+
+test("exceeding length does not blow up the handler",
+function() {
+  var view = Ember.View.create({
+    property: 'mystring',
+    template: Ember.Handlebars.compile('{{trim property length=100}}')
+  });
+  
+  Ember.run(function() {
+    view.appendTo('#qunit-fixture');
+  });
+  
+  equal(Ember.$('#qunit-fixture').text(), 'mystring', "it trims the string to given length");
+});
